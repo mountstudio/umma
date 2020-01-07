c64fc0d085634325b3ba848995b24901a4757053
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2020-01-07 19:26:38 +0600

    add our projects and header

---
+++ resources/views/blocks/header.blade.php
@@ -0,0 +1,51 @@
+<div class="container-fluid">
+    <div class="col-auto d-flex justify-content-end pt-2">
+        <h5 class="mr-2" style="color: #D57E2A">RU</h5>
+        <h5 style="color: #D57E2A">KG</h5>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-light shadow-none pt-0 ">
+
+        <div class="col-7 text-right">
+            <a class="navbar-brand" href="{{ route('welcome') }}"><img src="{{ asset('img/umma_logo.png') }}" alt=""
+                                                                       class="img-fluid justify-content-center"></a>
+        </div>
+        <div class="col-5 text-right m-0 p-0">
+            <a href=""><img class="mr-2" src="{{ asset('icons/facebook_icons.png') }}" alt=""></a>
+            <a href=""><img class="mr-2" src="{{ asset('icons/insta_icons.png') }}" alt=""></a>
+            <a href=""><img class="mr-2" src="{{ asset('icons/youtube_icons.png') }}" alt=""></a>
+            <a href=""><img class="" src="{{ asset('icons/rss_icons.png') }}" alt=""></a>
+        </div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+    </nav>
+</div>
+<ul class="d-flex" style="list-style: none;justify-content: space-between;">
+    <li class="nav-item ">
+        <a class="nav-link text-uppercase text-dark" href="" title="" style="">Надо знать</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link text-uppercase text-dark" href="" title="" style="">Это интересно</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link text-uppercase text-dark" href="" title="" style="">Просвещение</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link text-uppercase text-dark" href="" title="" style="">О наболевшем</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link text-uppercase text-dark" href="" title="" style="">Интервью</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link text-uppercase text-dark" href="" title="" style="">Медиа</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link text-uppercase text-dark" href="" title="" style="">Новости</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link text-uppercase text-dark" href="" title="" style="">Журнал</a>
+    </li>
+</ul>
---
+++ resources/views/blocks/our_projects.blade.php
@@ -0,0 +1,19 @@
+<div class="container">
+    <h1>Наши проекты</h1>
+    <div class="row text-center">
+        <div class="col-12">
+            <a href="">
+                <img src="{{ asset('img/ishker_aim.png') }}" class="img-fluid mr-4" alt="Ishker Aim">
+            </a>
+            <a href="">
+                <img src="{{ asset('img/islam_house.png') }}" class="img-fluid mr-4" alt="Islam House">
+            </a>
+            <a href="">
+                <img src="{{ asset('img/vilage.png') }}" class="img-fluid mr-4" alt="Vilage">
+            </a>
+            <a href="">
+                <img src="{{ asset('img/younth_action.png') }}" class="img-fluid mr-4" alt="Young Action">
+            </a>
+        </div>
+    </div>
+</div>
