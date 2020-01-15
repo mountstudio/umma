30731f9e5f0326c8b5ae1d5843d91fb3343516ff
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2020-01-15 18:50:10 +0600

    fx

--- resources/views/blocks/different.blade.php
+++ resources/views/blocks/different.blade.php
@@ -2,14 +2,26 @@
     <div class="row">
         <div class="col-3 px-3 pb-2">
             <h4 class="pl-3">Мир души</h4>
-            <div class="card">
+            <div class="card mb-2">
                 <img src="{{ asset('img/example-3.jpg') }}" class="card-img-top p-2" alt="...">
                 <div class="card-body text-center">
                     <img src="{{ asset('img/hello_html_m4e1bf07b.png') }}" style="width: 36%" alt="Узор">
                     <div class="row justify-content-center">
-                        <div class="col-6">
+                        <div class="col-12">
                             <h6 class="card-title text-uppercase">Трогательная история</h6>
-                            <p class="card-text text-uppercase">Покаяние малика ИБН Динар</p>
+                            <p class="card-text text-uppercase"  style="min-height: 52px;">Покаяние малика ИБН Динар</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card mb-2">
+                <img src="{{ asset('img/example-3.jpg') }}" class="card-img-top p-2" alt="...">
+                <div class="card-body text-center">
+                    <img src="{{ asset('img/hello_html_m4e1bf07b.png') }}" style="width: 36%" alt="Узор">
+                    <div class="row justify-content-center">
+                        <div class="col-12">
+                            <h6 class="card-title text-uppercase">Трогательная история</h6>
+                            <p class="card-text text-uppercase" style="min-height: 52px;">Покаяние малика ИБН Динар</p>
                         </div>
                     </div>
                 </div>
@@ -17,14 +29,26 @@
         </div>
         <div class="col-3 px-3 pb-2">
             <h4 class="pl-3">Люди в исламе</h4>
-            <div class="card">
+            <div class="card mb-2">
                 <img src="{{ asset('img/example-3.jpg') }}" class="card-img-top p-2" alt="...">
                 <div class="card-body text-center">
                     <img src="{{ asset('img/hello_html_m4e1bf07b.png') }}" style="width: 36%" alt="Узор">
                     <div class="row justify-content-center">
-                        <div class="col-6">
+                        <div class="col-12">
                             <h6 class="card-title text-uppercase">Мнение ученых</h6>
-                            <p class="card-text text-uppercase">Украденное слово "Аллаху Акбар"</p>
+                            <p class="card-text text-uppercase" style="min-height: 52px;">Украденное слово "Аллаху Акбар"</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card mb-2">
+                <img src="{{ asset('img/example-3.jpg') }}" class="card-img-top p-2" alt="...">
+                <div class="card-body text-center">
+                    <img src="{{ asset('img/hello_html_m4e1bf07b.png') }}" style="width: 36%" alt="Узор">
+                    <div class="row justify-content-center">
+                        <div class="col-12">
+                            <h6 class="card-title text-uppercase">Мнение ученых</h6>
+                            <p class="card-text text-uppercase" style="min-height: 52px;">Украденное слово "Аллаху Акбар"</p>
                         </div>
                     </div>
                 </div>
@@ -32,27 +56,49 @@
         </div>
         <div class="col-3 px-3 pb-2">
             <h4 class="pl-3">О наболевшом</h4>
-            <div class="card">
+            <div class="card mb-2">
                 <img src="{{ asset('img/example-3.jpg') }}" class="card-img-top p-2" alt="...">
                 <div class="card-body text-center">
                     <img src="{{ asset('img/hello_html_m4e1bf07b.png') }}"  style="width: 36%" alt="Узор">
                     <div class="row justify-content-center">
-                        <div class="col-6">
+                        <div class="col-12">
+                            <h6 class="text-uppercase">Сахабы</h6>
+                            <p class="card-text text-uppercase" style="min-height: 52px;">Праведный халиф Умар Ибн Хаттаб</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card mb-2">
+                <img src="{{ asset('img/example-3.jpg') }}" class="card-img-top p-2" alt="...">
+                <div class="card-body text-center">
+                    <img src="{{ asset('img/hello_html_m4e1bf07b.png') }}"  style="width: 36%" alt="Узор">
+                    <div class="row justify-content-center">
+                        <div class="col-12">
+                            <h6 class="text-uppercase">Сахабы</h6>
+                            <p class="card-text text-uppercase" style="min-height: 52px;">Праведный халиф Умар Ибн Хаттаб</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-2 px-3 pb-2">
+            <h4 class="pl-3">Журналы</h4>
+            <div class="card mb-3">
+                <img src="{{ asset('img/magazine_2017.png') }}"  class="card-img" alt="...">
+                <div class="card-img-overlay row m-0 align-items-end text-center ">
+                    <div class="row justify-content-center">
+                        <div class="col-12 rounded bg-black-50 py-3 text-white">
                             <h6 class="text-uppercase">Сахабы</h6>
                             <p class="card-text text-uppercase">Праведный халиф Умар Ибн Хаттаб</p>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="col-3 px-3 pb-2">
-            <h4 class="pl-3">Журналы</h4>
-            <div class="card">
-                <img src="{{ asset('img/magazine_2017.png') }}" style="height: 210px" class="card-img-top p-2" alt="...">
-                <div class="card-body text-center">
-                    <img src="{{ asset('img/hello_html_m4e1bf07b.png') }}"  style="width: 36%" alt="Узор">
+            <div class="card mb-3">
+                <img src="{{ asset('img/magazine_2017.png') }}"  class="card-img" alt="...">
+                <div class="card-img-overlay row m-0 align-items-end text-center ">
                     <div class="row justify-content-center">
-                        <div class="col-6">
+                        <div class="col-12 rounded bg-black-50 py-3 text-white">
                             <h6 class="text-uppercase">Сахабы</h6>
                             <p class="card-text text-uppercase">Праведный халиф Умар Ибн Хаттаб</p>
                         </div>
@@ -72,24 +118,6 @@
 {{--                </div>--}}
 {{--            </div>--}}
 {{--        @endfor--}}
-    </div>
-    <div class="row">
-        @for($i = 0; $i < 4; $i++)
-            <div class="col-3 px-3 pb-2">
-                <div class="card">
-                    <img src="{{ asset('img/example-3.jpg') }}" class="card-img-top p-2" alt="...">
-                    <div class="card-body text-center">
-                        <img src="{{ asset('img/hello_html_m4e1bf07b.png') }}" class="" style="width: 36%" alt="Узор">
-                        <div class="row justify-content-center">
-                            <div class="col-10">
-                                <h6 class="text-uppercase">Надо знать</h6>
-                                <p class="card-text text-uppercase">"ИНшаалах"- Когда это слово применить?</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        @endfor
     </div>
 </div>
 <div class="col-12 text-center  py-4">
--- resources/views/blocks/multimedia.blade.php
+++ resources/views/blocks/multimedia.blade.php
@@ -1,33 +1,33 @@
 <div class="container-fluid px-5">
     <h2 class="text-uppercase">Мультимедиа</h2>
     <div class="row">
-        <div class="col-12" style="background-color: orangered">
+        <div class="col-12 bg-orange">
             <div class="multimedia">
-                <div class="item border p-1 mr-1">
+                <div class="item border bg-for-image p-1 mr-1">
                     <img class="img-fluid" src="{{ asset('img/example-1.jpg') }}" alt="">
                 </div>
-                <div class="item border p-1 mr-1">
+                <div class="item border bg-for-image p-1 mr-1">
                     <img class="img-fluid" src="{{ asset('img/example-2.jpg') }}" alt="">
                 </div>
-                <div class="item border p-1 mr-1">
+                <div class="item border  bg-for-image p-1 mr-1">
                     <img class="img-fluid" src="{{ asset('img/example-3.jpg') }}" alt="">
                 </div>
-                <div class="item border p-1 mr-1">
+                <div class="item border  bg-for-image p-1 mr-1">
                     <img class="img-fluid" src="{{ asset('img/example-1.jpg') }}" alt="">
                 </div>
-                <div class="item border p-1 mr-1">
+                <div class="item border  bg-for-image p-1 mr-1">
                     <img class="img-fluid" src="{{ asset('img/example-2.jpg') }}" alt="">
                 </div>
-                <div class="item border p-1 mr-1">
+                <div class="item border  bg-for-image p-1 mr-1">
                     <img class="img-fluid" src="{{ asset('img/example-3.jpg') }}" alt="">
                 </div>
-                <div class="item border p-1 mr-1">
+                <div class="item border bg-for-image  p-1 mr-1">
                     <img class="img-fluid" src="{{ asset('img/example-1.jpg') }}" alt="">
                 </div>
-                <div class="item border p-1 mr-1">
+                <div class="item border  bg-for-image p-1 mr-1">
                     <img class="img-fluid" src="{{ asset('img/example-2.jpg') }}" alt="">
                 </div>
-                <div class="item border p-1 mr-1">
+                <div class="item border bg-for-image  p-1 mr-1">
                     <img class="img-fluid" src="{{ asset('img/example-3.jpg') }}" alt="">
                 </div>
             </div>
@@ -50,6 +50,9 @@
                 autoplaySpeed: 3000,
                 slidesToShow: 3,
                 slidesToScroll: 1,
+                arrows: true,
+                nextArrow: '<i class="fas fa-chevron-right"></i>',
+                prevArrow: '<i class="fas fa-chevron-left"></i>'
             });
 
         });
