87a6caf95442768fb1dd56d44d0d6a1846988939
Damir <Damir290598@gmail.com>
2020-02-11 18:28:11 +0600

    writed show models

---
+++ resources/views/admin/questions/show.blade.php
@@ -0,0 +1,8 @@
+@extends('admin.dashboard')
+@section('dashboard_content')
+    <div class="container">
+
+
+    </div>
+
+@endsection
---
+++ resources/views/admin/tags/create.blade.php
@@ -0,0 +1,8 @@
+@extends('admin.dashboard')
+@section('dashboard_content')
+    <div class="container">
+<h1>Tilya dalbaeb</h1>
+
+    </div>
+
+@endsection
