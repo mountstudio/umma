b2316a59e7c99234343b8199be8baa60dc4322bd
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2020-03-13 16:37:13 +0600

    fx show_media

--- resources/views/show_media.blade.php
+++ resources/views/show_media.blade.php
@@ -6,9 +6,6 @@
                 <nav aria-label="breadcrumb">
                 </nav>
                 <div class="author d-flex justify-content-between">
-                    <p>Автор:
-                        sdfsdfsdfsdf
-                    </p>
                     <span>345345</span>
                 </div>
                 <div class="post-header d-flex py-2">
@@ -27,20 +24,8 @@
                         </div>
                     </a>
                 </div>
-                <div class="d-flex py-2" style="background-color: #FFE8F8">
-                    <a href="https://www.instagram.com/ummamagkg/"><img class="px-3"
-                                                                        src="{{ asset('img/instagram-sketched (1).png') }}"
-                                                                        alt=""></a>
-                    <p class="p-1 m-0">Подписывайтесь на нашу страницу в <a href="https://www.instagram.com/ummamagkg/">Instagram</a>
-                    </p>
-                </div>
-                <div class="d-flex py-2" style="background-color: #FFE8F8">
-                    <a href="https://www.youtube.com/watch?v=pfab0uXYDpY&feature=youtu.be"><img class="px-3"
-                                                                        src="{{ asset('img/youtube (4).png') }}"
-                                                                        alt=""></a>
-                    <p class="p-1 m-0">Подписывайтесь на нашу страницу в <a href="https://www.youtube.com/watch?v=pfab0uXYDpY&feature=youtu.be">Youtube</a>
-                    </p>
-                </div>
+                @include('subscription.subscribe')
+                @include('share.share_buttons')
             </div>
             <div class="col-12 col-lg-4 pb-3">
                 @include('blocks.right-sidebar.new')
--- resources/views/subscription/subscribe.blade.php
+++ resources/views/subscription/subscribe.blade.php
@@ -5,3 +5,10 @@
     <p class="p-1 m-0">Подписывайтесь на нашу страницу в <a href="https://www.instagram.com/ummamagkg/">Instagram</a>
     </p>
 </div>
+<div class="d-flex py-2" style="background-color: #FFE8F8">
+    <a href="https://www.youtube.com/watch?v=pfab0uXYDpY&feature=youtu.be"><img class="px-3"
+                                                                                src="{{ asset('img/youtube (4).png') }}"
+                                                                                alt=""></a>
+    <p class="p-1 m-0">Подписывайтесь на нашу страницу в <a href="https://www.youtube.com/watch?v=pfab0uXYDpY&feature=youtu.be">Youtube</a>
+    </p>
+</div>
