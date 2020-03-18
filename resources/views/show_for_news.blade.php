e040ac706a830e41f5454d919422cf79530d6907
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2020-03-18 13:40:15 +0600

    add fotograph

--- resources/views/show_for_news.blade.php
+++ resources/views/show_for_news.blade.php
@@ -12,6 +12,9 @@
                             <a href="{{ route('show.author', $author) }}">{{ $author->full_name . ($loop->last ? '' : ',') }} </a>
                         @endforeach
                     </p>
+                    <p>
+                        Фотограф: Дамир Дамирович
+                    </p>
                     <span>{{$article->created_at->format('d.m.y')}}</span>
                 </div>
                 <div class="post-header d-flex py-2">
