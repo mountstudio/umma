d6f1f40576712a8f1f18ddff18b276efaa37d6b5
Uki7991 <tilek.kubanov@gmail.com>
2020-04-15 05:40:17 +0600

    Update second_card.blade.php

--- resources/views/articles/second_card.blade.php
+++ resources/views/articles/second_card.blade.php
@@ -7,8 +7,16 @@
            style="top: 10px; left: 10px;">{{ $article->category->name }}</p>
     </div>
     <div class="card-body py-2">
-        <small class="text-muted font-weight-bold py-3">{{ \Carbon\Carbon::make($article->created_at)->format('M d Y') }}</small>
-        <h3 class="h4 font-weight-bold card-title pt-3 m-0">{{ $article->name }}</h3>
-        <p class="card-text small" style="display: -webkit-box;-webkit-line-clamp: 3;-webkit-box-orient: vertical;overflow: hidden;text-overflow: ellipsis;">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <small
+            class="text-muted font-weight-bold py-3">{{ \Carbon\Carbon::make($article->created_at)->format('M d Y') }}</small>
+        <h3 class="h4 font-weight-bold card-title pt-3 m-0" style="display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;">{{ $article->name }}</h3>
+        <p class="card-text small"
+           style="display: -webkit-box;-webkit-line-clamp: 3;-webkit-box-orient: vertical;overflow: hidden;text-overflow: ellipsis;">
+            This is a wider card with supporting text below as a natural lead-in to additional content. This content is
+            a little bit longer.</p>
     </div>
 </div>
