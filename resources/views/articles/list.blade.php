01a6d89a4c057ad4abaf9a5906699868fa6eb9e5
Uki7991 <tilek.kubanov@gmail.com>
2020-04-15 06:29:34 +0600

    Update list.blade.php

--- resources/views/articles/list.blade.php
+++ resources/views/articles/list.blade.php
@@ -1,5 +1,5 @@
 @foreach($articles->chunk(2) as $articleChunk)
-    <div class="col-12 col-lg-{{ $articleChunk->count() == 1 ? "6" : '12' }} col-md-12 pb-4">
+    <div class="col-12 col-lg-{{ $articleChunk->count() == 1 ? "6" : '12' }} col-md-{{ $articleChunk->count() == 1 ? "6" : '12' }} pb-4">
         <div class="card-deck">
             @foreach($articleChunk as $article)
                 @include('articles.card')
